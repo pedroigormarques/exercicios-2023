43c8d236c61f003ef4891f915bfc333e9dca6225
Pedro Igor <pedroigor.marques@gmail.com>
2023-10-06 11:17:34 -0300

    Adição do sistema para o título do trabalho

--- ts/src/app/app.component.ts
+++ ts/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'DevChuva';
+export class AppComponent implements OnInit {
+  title =
+    'Análise sensorial de preparações funcionais desenvolvidas para escolares entre 09 e 15 anos, do município de Campinas/SP';
+  constructor(private readonly titleService: Title) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+  }
 }
